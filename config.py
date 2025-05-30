8a17fc0f2667e683a5cc2eb6ce4ef1719f4f8486
shivensh <shivenshmeena0752@gmail.com>
2025-05-30 14:20:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api id here"
-api_hash = "enter your api hash here"
-bot_token = "enter your bot token here"
-auth_users = [enter your auth user id here]
+api_id = "2695501"
+api_hash = "9a544068ea55f54ca7725245d0e8f0f9"
+bot_token = "7763372293:AAGhc-KPOTjG3wuP2zK_Za1HfK1raU039NQ"
+auth_users = [8049118708]
