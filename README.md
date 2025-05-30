4518c1e0837dc09ccee63c5ad5c3ddb4776550df
shivensh <shivenshmeena0752@gmail.com>
2025-05-30 14:13:39 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+NOTE - I do not support piracy. This tool is just for educational purposes
---
+++ config.py
@@ -0,0 +1,4 @@
+api_id = "enter your api id here"
+api_hash = "enter your api hash here"
+bot_token = "enter your bot token here"
+auth_users = [enter your auth user id here]
