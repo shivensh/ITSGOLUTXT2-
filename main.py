14e068df8872d2adf187b5e2e3e276c6ea69c2e1
shivensh <shivenshmeena0752@gmail.com>
2025-05-30 14:22:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 image_list = [
-"https://graph.org/file/8b1f4146a8d6b43e5b2bc-be490579da043504d5.jpg",
-"https://graph.org/file/b75dab2b3f7eaff612391-282aa53538fd3198d4.jpg",
-"https://graph.org/file/38de0b45dd9144e524a33-0205892dd05593774b.jpg",
-"https://graph.org/file/be39f0eebb9b66d7d6bc9-59af2f46a4a8c510b7.jpg",
-"https://graph.org/file/8b7e3d10e362a2850ba0a-f7c7c46e9f4f50b10b.jpg",
+"https://i.ibb.co/0RpjC988/photo-2022-01-27-00-29-02.jpg",
 ]
 print(4321)
 bot = Client(
